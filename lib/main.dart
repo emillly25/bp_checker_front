2f01df557723bd0f4b67cf8297c223679f0c1b8b
SEHEE <89740574+emillly25@users.noreply.github.com>
2023-05-01 11:12:32 +0900

    서버랑 통신 연결

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'BP Chekcer_v1',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const BPChecker(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class BPChecker extends StatefulWidget {
+  const BPChecker({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<BPChecker> createState() => _BPCheckerState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _BPCheckerState extends State<BPChecker> {
+  String result = '';
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Future testPost() async {
+    //testpost
+    final String url = '${dotenv.env['SERVER_URL']}/user';
+    final obj = {'name': result};
+    var response = await http.post(
+      Uri.parse(url),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: json.encode(obj),
+    );
+    print('Response status: ${response.statusCode}');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('혈압 측정기록'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Container(
+        padding: EdgeInsets.all(15),
+        child: Center(
+          child: Column(children: [
+            TextFormField(
+              onChanged: (value) => {result = value},
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(height: 20),
+            OutlinedButton(
+              onPressed: () {
+                testPost();
+              },
+              child: Text('저장'),
             ),
-          ],
+          ]),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
